acadbc1b7152bb6505bcdd8f6c2cd33307f236b7
tushar-454 <imtushar454@gmail.com>
2024-11-29 21:33:06 +0600

    refactor: replace throwError utility with createError; update token creation error handling

--- src/utils/createToken.js
+++ src/utils/createToken.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
-const throwError = require('./throwError');
+const createError = require('./createError');
 
 const createToken = (payload) => {
   try {
     const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
     return token;
   } catch (error) {
-    throwError(error);
+    createError(error.message, error.status);
   }
 };
 
--- src/utils/throwError.js
+++
@@ -1,7 +0,0 @@
-const throwError = (error, status) => {
-  const err = new Error(error);
-  err.status = status;
-  throw err;
-};
-
-module.exports = throwError;
